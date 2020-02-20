5bb500b4f58581ed9c4e3fb1d96f1a2fe634088c
Adam Demuri <adam.demuri@gmail.com>
2020-02-19 21:25:03 -0700

    Log the current CO2 level to the OLED display

--- co2/co2.ino
+++ co2/co2.ino
@@ -1,17 +1,21 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
 #include <MHZ19.h>
+#include <SPI.h>
+#include <Wire.h>
 
 MHZ19 sensor;
+Adafruit_SSD1306 display(128, 64, &Wire, /* oled reset */ -1);
 
 void debug_printf(const char *fmt, ...) {
-	static char buf[256];  // Statically allocate 256 bytes for printf'ing.
+  static char buf[256];  // Statically allocate 256 bytes for printf'ing.
   va_list args;
   va_start(args, fmt);
   vsnprintf(buf, 128, fmt, args);
   va_end(args);
   Serial.print(buf);
 }
-
 
 void setup() {
   Serial.begin(115200);
@@ -23,10 +27,32 @@ void setup() {
   sensor.begin(Serial1);
   sensor.autoCalibration(false);
 
+  Wire.begin(5, 4);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    while (true) {
+      Serial.println(F("SSD1306 allocation failed"));
+      delay(100);
+    }
+  }
+  display.clearDisplay();
+  display.setTextSize(5);               // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.cp437(true);  // Use full 256 char 'Code Page 437' fontlay(100);
+
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
   debug_printf("co2: %d\n", sensor.getCO2(), sensor.getTemperature());
+
+  display.clearDisplay();
+  display.setCursor(0, 12);  // Start at top-left corner
+
+  static char buf[16];
+  snprintf(buf, 16, "%4d", sensor.getCO2());
+  for (int i = 0; i < 4; i++) {
+    display.write(buf[i]);
+  }
+  display.display();
   delay(100);
 }
