b4d240929f7dc468b10170d13e5621e8d7f80dbf
Adam Demuri <adam.demuri@gmail.com>
2020-02-20 18:59:46 -0700

    Display warmup progress bar.
    Also enable filtering, which will display 0 instead of garbage values
    when the sensor is starting.

--- co2/co2.ino
+++ co2/co2.ino
@@ -5,10 +5,13 @@
 #include <SPI.h>
 #include <Wire.h>
 
-MHZ19 sensor;
-Adafruit_SSD1306 display(128, 64, &Wire, /* oled reset */ -1);
+static const uint32_t kWarmupTime = 3 * 60 * 1000;
+static const int16_t kScreenWidth = 128;
 
-void debug_printf(const char *fmt, ...) {
+MHZ19 sensor;
+Adafruit_SSD1306 display(kScreenWidth, /* height */ 64, &Wire, /* oled reset */ -1);
+
+void serial_printf(const char *fmt, ...) {
   static char buf[256];  // Statically allocate 256 bytes for printf'ing.
   va_list args;
   va_start(args, fmt);
@@ -17,15 +20,30 @@ void debug_printf(const char *fmt, ...) {
   Serial.print(buf);
 }
 
+void display_co2(int ppm) {
+  display.setCursor(0, 16);  // Start at top-left corner
+
+  static char buf[5];
+  snprintf(buf, 5, "%4d", ppm);
+  for (int i = 0; i < 4; i++) {
+    display.write(buf[i]);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
+  // Note: this LED is inverted
   digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  delay(1);
 
   Serial1.begin(9600, SERIAL_8N1, 22, 23);
   sensor.begin(Serial1);
+  // Don't auto calibrate, since that assumes that the sensor will be exposed
+  // to 400 PPM (outside) air for at least 20m for each 24h on.
   sensor.autoCalibration(false);
+  // Return 0 instead of garbage values when the sensor is initializing.
+  sensor.setFilter();
 
   Wire.begin(5, 4);
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -35,24 +53,29 @@ void setup() {
     }
   }
   display.clearDisplay();
-  display.setTextSize(5);               // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE);  // Draw white text
+  display.setTextSize(5);
+  display.setTextColor(SSD1306_WHITE);  // Draw white text (it's the only option)
   display.cp437(true);  // Use full 256 char 'Code Page 437' fontlay(100);
 
   digitalWrite(LED_BUILTIN, HIGH);
+
+  // The sensor takes 3 minutes to warm up - show a timer bar while this happens
+  while (millis() < kWarmupTime) {
+    display.clearDisplay();
+
+    int16_t width = millis() * kScreenWidth / kWarmupTime;
+    display_co2(sensor.getCO2());
+    display.fillRect(0, 0, width, 4, SSD1306_WHITE);
+    display.display();
+    delay(100);
+  }
+
+  display.setTextColor(SSD1306_WHITE);  // Draw white text
 }
 
 void loop() {
-  debug_printf("co2: %d\n", sensor.getCO2(), sensor.getTemperature());
-
   display.clearDisplay();
-  display.setCursor(0, 12);  // Start at top-left corner
-
-  static char buf[16];
-  snprintf(buf, 16, "%4d", sensor.getCO2());
-  for (int i = 0; i < 4; i++) {
-    display.write(buf[i]);
-  }
+  display_co2(sensor.getCO2());
   display.display();
   delay(100);
 }
