52c1ac60e49b3f178706b3ac538956a4a78364a9
Adam Demuri <adam.demuri@gmail.com>
2020-02-20 19:58:30 -0700

    Connect to Adafruit IO and log values

--- co2/co2.ino
+++ co2/co2.ino
@@ -1,15 +1,30 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
-#include <MHZ19.h>
+
+// These deps are needed by dependent libraries. Due to some PlatformIO bug, the libraries can only find them if they're #included here first.
+#include <Adafruit_GFX.h>
 #include <SPI.h>
+#include <WiFiClientSecure.h>
+
+#include <Adafruit_SSD1306.h>
+#include <AdafruitIO.h>
+#include <AdafruitIO_WiFi.h>
+#include <MHZ19.h>
 #include <Wire.h>
+#include <WiFi.h>
+
+#include "secrets.h"
 
 static const uint32_t kWarmupTime = 3 * 60 * 1000;
 static const int16_t kScreenWidth = 128;
+// Sensor refreshes every 5s
+static const uint32_t kLogDelay = 5 * 1000;
 
 MHZ19 sensor;
 Adafruit_SSD1306 display(kScreenWidth, /* height */ 64, &Wire, /* oled reset */ -1);
+AdafruitIO_WiFi io(kIoUsername, kIoKey, kSsid, kSsidPassword);
+AdafruitIO_Feed *feed;
+
+uint32_t log_at = 0;
 
 void serial_printf(const char *fmt, ...) {
   static char buf[256];  // Statically allocate 256 bytes for printf'ing.
@@ -57,14 +72,32 @@ void setup() {
   display.setTextColor(SSD1306_WHITE);  // Draw white text (it's the only option)
   display.cp437(true);  // Use full 256 char 'Code Page 437' fontlay(100);
 
+  // connect to io.adafruit.com
+  io.connect();
+  // wait for a connection
+  while(io.status() < AIO_CONNECTED) {
+    static int pos = 0;
+    display.clearDisplay();
+    display.setCursor(pos % 2 ? 10 : 0, 0);
+    display.write('i');
+    display.write('o');
+    display.display();
+    delay(100);
+    pos++;
+  }
+  feed = io.feed("co2");
+
   digitalWrite(LED_BUILTIN, HIGH);
 
   // The sensor takes 3 minutes to warm up - show a timer bar while this happens
-  while (millis() < kWarmupTime) {
+  int co2 = 0;
+  while (millis() < kWarmupTime || co2 == 0) {
+    co2 = sensor.getCO2();
+
     display.clearDisplay();
 
     int16_t width = millis() * kScreenWidth / kWarmupTime;
-    display_co2(sensor.getCO2());
+    display_co2(co2);
     display.fillRect(0, 0, width, 4, SSD1306_WHITE);
     display.display();
     delay(100);
@@ -74,8 +107,19 @@ void setup() {
 }
 
 void loop() {
+  io.run();
+  int co2 = sensor.getCO2();
+
   display.clearDisplay();
-  display_co2(sensor.getCO2());
+  display_co2(co2);
+
+  if (millis() > log_at) {
+    feed->save(co2);
+    log_at = millis() + kLogDelay;
+    display.fillCircle(8, 120, 4, SSD1306_WHITE);
+  }
+
   display.display();
-  delay(100);
+
+  delay(500);
 }
---
+++ co2/secrets.sample.h
@@ -0,0 +1,13 @@
+// Copy this file to secrets.h and fill out the values.
+
+#ifndef SECRETS
+#define SECRETS
+
+static const char* kSsid = "";
+static const char* kSsidPassword = "";
+
+static const char* kIoUsername = "";
+static const char* kIoKey = "";
+
+#endif
+
